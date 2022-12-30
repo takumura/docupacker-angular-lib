f039a8d79d6987af0b9e8ff53f7456f3ae508be7
takumura <takumura@outlook.com>
2022-12-30 16:42:37 +0900

    chore: adjust config setting

--- projects/docupacker-angular-lib/src/lib/docupacker-angular-lib.module.ts
+++ projects/docupacker-angular-lib/src/lib/docupacker-angular-lib.module.ts
@@ -1,16 +1,9 @@
 import { NgModule } from '@angular/core';
 import { DocupackerAngularLibComponent } from './docupacker-angular-lib.component';
 
-
-
 @NgModule({
-  declarations: [
-    DocupackerAngularLibComponent
-  ],
-  imports: [
-  ],
-  exports: [
-    DocupackerAngularLibComponent
-  ]
+  declarations: [DocupackerAngularLibComponent],
+  imports: [],
+  exports: [DocupackerAngularLibComponent],
 })
-export class DocupackerAngularLibModule { }
+export class DocupackerAngularLibModule {}
